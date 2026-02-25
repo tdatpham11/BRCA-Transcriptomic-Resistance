97f1341ac90b6ed470364e80774647e9b4d07856
tdatpham11 <phamdat1162000@gmail.com>
2026-02-25 10:40:01 +0700

    Update 03_plots.R

--- scripts/03_plots.R
+++ scripts/03_plots.R
@@ -14,3 +14,48 @@ plot <- EnhancedVolcano(res,
 
 ggsave("results/plots/volcano_plot.png", plot, width = 7, height = 7)
 print("Volcano Plot generated in results/plots/")
+library(DESeq2)
+library(ggplot2)
+
+# 1. Transform data for visualization
+# Note: Use blind = FALSE so the transformation isn't biased by your groups
+vsd <- vst(dds, blind = FALSE)
+
+# 2. Generate PCA Plot
+pca_data <- plotPCA(vsd, intgroup = "definition", returnData = TRUE)
+percentVar <- round(100 * attr(pca_data, "percentVar"))
+
+ggplot(pca_data, aes(PC1, PC2, color = definition)) +
+  geom_point(size = 3) +
+  xlab(paste0("PC1: ", percentVar[1], "% variance")) +
+  ylab(paste0("PC2: ", percentVar[2], "% variance")) +
+  coord_fixed() +
+  theme_minimal() +
+  ggtitle("PCA: Tumor vs Normal Samples")
+
+ggsave("results/plots/pca_plot.png")
+library(pheatmap)
+
+# 1. Get the top 50 significant genes by adjusted p-value
+top_genes <- head(order(res$padj), 50)
+mat <- assay(vsd)[top_genes, ]
+
+# 2. Subtract the row mean for better visualization (Z-score like)
+mat <- mat - rowMeans(mat)
+
+# 3. Create Annotation for columns (Tumor vs Normal)
+df <- as.data.frame(colData(dds)[, c("definition")])
+rownames(df) <- colnames(dds)
+colnames(df) <- "Type"
+
+# 4. Draw the Heatmap
+pheatmap(mat, 
+         annotation_col = df, 
+         show_colnames = FALSE, 
+         cluster_cols = TRUE, 
+         main = "Top 50 Differentially Expressed Genes",
+         color = colorRampPalette(c("navy", "white", "firebrick3"))(50))
+
+# Note: pheatmap doesn't use ggsave. Use this instead:
+dev.copy(png, "results/plots/heatmap_top50.png")
+dev.off()
